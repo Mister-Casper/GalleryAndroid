874b043759e32238c12a74100fa736f3af167f94
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-15 11:59:49 +0700

    Перемещены

---
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewAllImages/view/ViewAllImages.java
@@ -0,0 +1,21 @@
+package com.journaldev.mvpdagger2.activity.ViewAllImages.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.journaldev.mvpdagger2.R;
+
+public class ViewAllImages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewallimages);
+
+    }
+
+}
