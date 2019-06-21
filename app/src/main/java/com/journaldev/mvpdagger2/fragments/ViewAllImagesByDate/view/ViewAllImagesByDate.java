aa12399596778849feab989fb95c16c917793456
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-21 21:12:10 +0700

    Изменен

--- app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate/view/ViewAllImagesByDate.java
+++ app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate/view/ViewAllImagesByDate.java
@@ -5,12 +5,15 @@ package com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate.view;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.journaldev.mvpdagger2.Data.ImageUrls;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.adapters.GridPhotoAdapter;
 import com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate.model.ItemDate;
@@ -39,6 +42,11 @@ public class ViewAllImagesByDate extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -53,16 +61,19 @@ public class ViewAllImagesByDate extends Fragment {
     public void onStart() {
         super.onStart();
         model.init(getContext());
-        Uri[] uri = ViewAllImagesByDateModel.getUrls();
-        long[] date = ViewAllImagesByDateModel.getDate();
-        ItemDate photo = new ItemDate("545", uri);
+
+        Uri[] uri = ImageUrls.getUrls();
+        ItemDate photo = new ItemDate(uri);
 
         final ArrayList<ItemDate> arrayList = new ArrayList<>();
         arrayList.add(photo);
 
-        int a = DataList.getWidth();
         GridPhotoAdapter adapter = new GridPhotoAdapter(getActivity().getApplicationContext(), arrayList);
         DataList.setAdapter(adapter);
+    }
+
+    private void complianceUrlCheck()
+    {
 
     }
 
