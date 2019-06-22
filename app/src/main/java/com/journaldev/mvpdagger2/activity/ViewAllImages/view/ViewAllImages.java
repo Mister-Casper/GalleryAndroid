1990b290b130a5280534dc615c8f3a3a8b3144df
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-22 18:07:23 +0700

    Убран неиспользуемый код

--- app/src/main/java/com/journaldev/mvpdagger2/activity/ViewAllImages/view/ViewAllImages.java
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewAllImages/view/ViewAllImages.java
@@ -28,19 +28,12 @@ public class ViewAllImages extends AppCompatActivity {
         setContentView(R.layout.viewallimages);
         ButterKnife.bind(this);
 
-       //     ActivityCompat.requestPermissions(ViewAllImages.this,
-           //         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-// Create new fragment and transaction
         ViewAllImagesByDate newFragment = new ViewAllImagesByDate();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-// Replace whatever is in the fragment_container view with this fragment,
-// and add the transaction to the back stack if needed
         transaction.replace(R.id.frame, newFragment);
         transaction.addToBackStack(null);
 
-// Commit the transaction
         transaction.commit();
     }
 
