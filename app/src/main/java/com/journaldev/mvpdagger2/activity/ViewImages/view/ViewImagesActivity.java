7bff1e2466fd3eae6c75ae2ff9867da6f1da5fc2
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-22 11:55:00 +0700

    Изменен дизайн

--- app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
@@ -1,11 +1,18 @@
 package com.journaldev.mvpdagger2.activity.ViewImages.view;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.Context;
 import android.net.Uri;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -23,11 +30,6 @@ import butterknife.OnClick;
 
 public class ViewImagesActivity extends AppCompatActivity implements ViewImagesContract.ViewCallBack {
 
-
-    @BindView(R.id.backImageButton)
-    Button backImageButton;
-    @BindView(R.id.nextImageButton)
-    Button nextImageButton;
     @BindView(R.id.image)
     ImageView image;
 
@@ -36,6 +38,12 @@ public class ViewImagesActivity extends AppCompatActivity implements ViewImagesC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
         setContentView(R.layout.viewimages);
         ButterKnife.bind(this);
 
@@ -43,8 +51,15 @@ public class ViewImagesActivity extends AppCompatActivity implements ViewImagesC
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 
         initPresenter();
-        presenter.chandgeCurrentImage(0);
+        if(savedInstanceState == null)
+        selectStandartImage();
         image.setOnTouchListener(new OnSwipeTouchListener(ViewImagesActivity.this));
+    }
+
+
+    private void selectStandartImage() {
+        int newStartImageId = getIntent().getIntExtra("idImage", 0);
+        presenter.setCurrentImage(newStartImageId);
     }
 
     private void initPresenter() {
@@ -58,15 +73,14 @@ public class ViewImagesActivity extends AppCompatActivity implements ViewImagesC
         image.setImageURI(imageUri);
     }
 
-    @OnClick(R.id.backImageButton)
     public void onBackImageButton() {
         presenter.chandgeCurrentImage(-1);
     }
 
-    @OnClick(R.id.nextImageButton)
     public void onNextImageButton() {
         presenter.chandgeCurrentImage(1);
     }
+
 
     @Override
     public Context getContext() {
