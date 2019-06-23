03419fa19929e3bdbcd225f5a64164f8e3b3f48e
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-23 15:39:56 +0700

    Добавлен адаптер для работы вкладок

---
+++ app/src/main/java/com/journaldev/mvpdagger2/adapters/FragmentPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.journaldev.mvpdagger2.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate.ViewAllImagesByDate;
+import com.journaldev.mvpdagger2.fragments.albums.albums;
+
+public class FragmentPagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+    Fragment[] fragments = new Fragment[2];
+
+    public FragmentPagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+        fragments[0] = new ViewAllImagesByDate();
+        fragments[1] = new albums();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
