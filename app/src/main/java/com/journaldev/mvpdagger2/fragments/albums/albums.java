3dd544676d9b984bc453e4e02772d829f0304466
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-23 15:39:42 +0700

    Добавлен фрагмент для альбомов

---
+++ app/src/main/java/com/journaldev/mvpdagger2/fragments/albums/albums.java
@@ -0,0 +1,33 @@
+package com.journaldev.mvpdagger2.fragments.albums;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.journaldev.mvpdagger2.R;
+
+
+public class albums extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_albums, container, false);
+    }
+
+
+}
