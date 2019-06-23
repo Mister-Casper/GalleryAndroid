c4de886173f23266060b5bec3c22d0f89622b4b7
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-23 21:35:11 +0700

    Добавлена возможность запуска моего приложения из стороннего

--- app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
@@ -2,6 +2,7 @@ package com.journaldev.mvpdagger2.activity.ViewImages.view;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -12,9 +13,12 @@ import android.view.ScaleGestureDetector;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.journaldev.mvpdagger2.Data.ImageUrls;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.activity.ViewImages.model.ViewImagesModel;
 import com.journaldev.mvpdagger2.adapters.ImagesPageAdapter;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,12 +37,38 @@ public class ViewImagesActivity extends AppCompatActivity {
         allScreen();
         setContentView(R.layout.viewimages);
         ButterKnife.bind(this);
-
         ImagesPageAdapter mCustomPagerAdapter = new ImagesPageAdapter(this);
         pager.setAdapter(mCustomPagerAdapter);
         pager.setOffscreenPageLimit(3);
         if (savedInstanceState == null)
             selectStandartImage();
+        getOtherIntent();
+    }
+
+    private void getOtherIntent() {
+        Uri imageUri = null;
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_VIEW.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                imageUri = intent.getData();
+                Uri[] uri = ImageUrls.getUrls(getApplicationContext());
+                for (int i = 0; i < uri.length; i++) {
+                    File file = new File(String.valueOf(uri[i]));
+                    Uri temp = Uri.fromFile(file);
+                    if (imageUri.equals(temp)) ;
+                    {
+                        pager.setCurrentItem(i);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void startActivity(Intent intent) {
+
     }
 
     private void allScreen() {
