112268a945c9aff0312e8036858eab583869e98c
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-06-25 10:28:34 +0700

    Добавлено контекстное меню

--- app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewImages/view/ViewImagesActivity.java
@@ -1,21 +1,20 @@
 package com.journaldev.mvpdagger2.activity.ViewImages.view;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.journaldev.mvpdagger2.Data.ImageUrls;
 import com.journaldev.mvpdagger2.R;
-import com.journaldev.mvpdagger2.activity.ViewImages.model.ViewImagesModel;
+import com.journaldev.mvpdagger2.activity.FileInfo.FIleInfo;
 import com.journaldev.mvpdagger2.adapters.ImagesPageAdapter;
 
 import java.io.File;
@@ -26,13 +25,12 @@ import butterknife.ButterKnife;
 
 public class ViewImagesActivity extends AppCompatActivity {
 
-
-    //  ViewImagesContract.PresenterCallBack presenter;
     @BindView(R.id.pager)
     ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("");
         super.onCreate(savedInstanceState);
         allScreen();
         setContentView(R.layout.viewimages);
@@ -43,6 +41,30 @@ public class ViewImagesActivity extends AppCompatActivity {
         if (savedInstanceState == null)
             selectStandartImage();
         getOtherIntent();
+        ColorDrawable abDrawable = new ColorDrawable(getResources().getColor(R.color.gray));
+        abDrawable.setAlpha(0);
+        getSupportActionBar().setBackgroundDrawable(abDrawable);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.file_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+                Intent intent = new Intent(getApplicationContext(), FIleInfo.class);
+                Uri currentUri = ImageUrls.getImage(pager.getCurrentItem());
+                intent.putExtra("uri", currentUri.toString());
+                getApplicationContext().startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getOtherIntent() {
@@ -67,14 +89,8 @@ public class ViewImagesActivity extends AppCompatActivity {
         }
     }
 
-    public void startActivity(Intent intent) {
-
-    }
-
     private void allScreen() {
         Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
