8fbf929daf61ad695fea49c13f30004d265f3068
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-07-07 21:53:09 +0700

    Добавлен фрагмент для отображения списка альбомов

--- app/src/main/java/com/journaldev/mvpdagger2/fragments/albums/albums.java
+++ app/src/main/java/com/journaldev/mvpdagger2/fragments/albums/albums.java
@@ -1,20 +1,27 @@
 package com.journaldev.mvpdagger2.fragments.albums;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import com.journaldev.mvpdagger2.Data.AlbumsInfo;
 import com.journaldev.mvpdagger2.R;
+import com.journaldev.mvpdagger2.adapters.GridAlbomsAdapter;
+import com.journaldev.mvpdagger2.adapters.GridPhotoAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 public class albums extends Fragment {
 
-
-
+    @BindView(R.id.field)
+    GridView field;
+    Unbinder unbinder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,20 @@ public class albums extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_albums, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_albums, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        AlbumsInfo.loadImageUrl(getContext());
+        AlbumsInfo.Album[] albums = AlbumsInfo.getAllAlbum();
+        GridAlbomsAdapter adapter = new GridAlbomsAdapter(getActivity(), albums);
+        field.setAdapter(adapter);
+        return view;
     }
 
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
