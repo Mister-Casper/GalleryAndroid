9fe5c8d6ccfdc1e8fceabb5c85a527ce08d709ce
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-07-07 21:53:57 +0700

    Переименован

--- app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate/ViewAllImagesByDate.java
+++ app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate/ViewAllImagesByDate.java
@@ -4,22 +4,18 @@ package com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate;
 //grid:layout_columnWeight="1"
 
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.journaldev.mvpdagger2.Data.ImageUrls;
+import com.journaldev.mvpdagger2.Data.ItemPhotoData;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.adapters.GridPhotoAdapter;
-import com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate.ItemDate;
 
 
 import java.util.ArrayList;
@@ -55,9 +51,9 @@ public class ViewAllImagesByDate extends Fragment {
         View view = inflater.inflate(R.layout.fragmentviewallimagesbydate, container, false);
         unbinder = ButterKnife.bind(this, view);
         Uri[] uri = ImageUrls.getUrls(getContext());
-        ItemDate photo = new ItemDate(uri);
+        ItemPhotoData photo = new ItemPhotoData(uri);
 
-        final ArrayList<ItemDate> arrayList = new ArrayList<>();
+        final ArrayList<ItemPhotoData> arrayList = new ArrayList<>();
         arrayList.add(photo);
 
         GridPhotoAdapter adapter = new GridPhotoAdapter(getActivity().getApplicationContext(), arrayList);
