727cb6606bde44da187882ac28c3114d617d221a
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-07-08 14:00:03 +0700

    Исправлен баг

--- app/src/main/java/com/journaldev/mvpdagger2/activity/ViewAllImages/view/ViewAllImages.java
+++ app/src/main/java/com/journaldev/mvpdagger2/activity/ViewAllImages/view/ViewAllImages.java
@@ -1,21 +1,33 @@
 package com.journaldev.mvpdagger2.activity.ViewAllImages.view;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.adapters.FragmentPagerAdapter;
+import com.journaldev.mvpdagger2.utils.FabricEvents;
+import com.journaldev.mvpdagger2.utils.MeasurementLaunchTime;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.fabric.sdk.android.Fabric;
 
 public class ViewAllImages extends AppCompatActivity {
 
@@ -29,6 +41,28 @@ public class ViewAllImages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("");
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadActivity();
+                } else {
+                    throw new SecurityException();
+                }
+                return;
+            }
+        }
+    }
+
+    private void loadActivity() {
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.viewallimages);
         ButterKnife.bind(this);
         final FragmentPagerAdapter adapter = new FragmentPagerAdapter
@@ -38,8 +72,8 @@ public class ViewAllImages extends AppCompatActivity {
         setTableSelector();
     }
 
-    private void setTableSelector()
-    {
+
+    private void setTableSelector() {
         tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
