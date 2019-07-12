bb4449d67b9f6f681c6052cb851cbdb50da45af5
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-07-12 23:13:13 +0700

    Update ItemPhotoData.java

--- app/src/main/java/com/journaldev/mvpdagger2/Data/ItemPhotoData.java
+++ app/src/main/java/com/journaldev/mvpdagger2/Data/ItemPhotoData.java
@@ -4,13 +4,13 @@ import android.net.Uri;
 
 public class ItemPhotoData {
 
-    public Uri[] getPhoto() {
+    public Uri getPhoto() {
         return photo;
     }
 
-    private Uri[] photo;
+    private Uri photo;
 
-    public ItemPhotoData(Uri[] photo) {
+    public ItemPhotoData(Uri photo) {
         this.photo = photo;
     }
 }
