150994dde805cb77dbe4879fc23b87cfdcdba6cb
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-07-12 23:11:27 +0700

    Заменен GridView на RecycleView

--- app/src/main/java/com/journaldev/mvpdagger2/adapters/AlbumsAdapter.java
+++ app/src/main/java/com/journaldev/mvpdagger2/adapters/AlbumsAdapter.java
@@ -3,6 +3,8 @@ package com.journaldev.mvpdagger2.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +13,86 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.journaldev.mvpdagger2.Data.AlbumsInfo;
+import com.journaldev.mvpdagger2.Data.ItemPhotoData;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.activity.ViewImages.view.ViewImagesActivity;
 import com.journaldev.mvpdagger2.myVIew.MyImageView;
-import com.journaldev.mvpdagger2.utils.SquareImageView;
 
 import java.io.File;
 import java.util.ArrayList;
 
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.ViewHolder> {
+
+    private AlbumsInfo.Album[] mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public AlbumsAdapter(Context context, AlbumsInfo.Album[] albums) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = albums;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public AlbumsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.albumitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumsAdapter.ViewHolder holder, int position) {
+        Uri albumPhoto = mData[position].getUri().get(0);
+        String AlbumName = mData[position].getName();
+        File file = new File(String.valueOf(albumPhoto));
+        Uri uri = Uri.fromFile(file);
+        Glide.with(mInflater.getContext()).load(uri).into(holder.image);
+        holder.albumName.setText(AlbumName);
+    }
+
+    // total number of cells
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        MyImageView image;
+        TextView albumName;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.picture);
+            albumName = itemView.findViewById(R.id.text);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    AlbumsInfo.Album getItem(int id) {
+        return mData[id];
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
+
+/*
 public class GridAlbomsAdapter extends BaseAdapter {
     Context ctx;
     LayoutInflater lInflater;
@@ -95,3 +169,4 @@ public class GridAlbomsAdapter extends BaseAdapter {
 
 }
 
+*/
---
+++ app/src/main/java/com/journaldev/mvpdagger2/adapters/PhotosAdapter.java
@@ -0,0 +1,85 @@
+package com.journaldev.mvpdagger2.adapters;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.journaldev.mvpdagger2.Data.ItemPhotoData;
+import com.journaldev.mvpdagger2.R;
+import com.journaldev.mvpdagger2.myVIew.MyImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.ViewHolder> {
+
+    private ArrayList<ItemPhotoData> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+   public PhotosAdapter(Context context, ArrayList<ItemPhotoData> products) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = products;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.imageitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Uri photo = mData.get(position).getPhoto();
+        File file = new File(String.valueOf(photo));
+        Uri uri = Uri.fromFile(file);
+        Glide.with(mInflater.getContext()).load(uri).into(holder.image);
+        holder.image.setPadding(3,3,3,3);
+    }
+
+    // total number of cells
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        MyImageView image;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.picture);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    ItemPhotoData getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
