2cf889dae29561c93743330d0d9015ccdb0799f1
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-12 21:21:48 +0700

    Очищен

--- app/src/main/java/com/journaldev/mvpdagger2/myVIew/zoomImageView.java
+++ app/src/main/java/com/journaldev/mvpdagger2/myVIew/zoomImageView.java
@@ -1,36 +1,7 @@
 package com.journaldev.mvpdagger2.myVIew;
 
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.res.Resources;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.ThumbnailUtils;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
-import android.widget.GridView;
-import android.widget.ImageView;
-
-import com.davemorrissey.labs.subscaleview.ImageSource;
-import com.journaldev.mvpdagger2.R;
-import com.journaldev.mvpdagger2.utils.FabricEvents;
-import com.journaldev.mvpdagger2.utils.MeasurementLaunchTime;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
