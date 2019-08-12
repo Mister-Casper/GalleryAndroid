a65c335561ce28560bcacb05733faf6dea3b6dcf
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-12 21:22:14 +0700

    Revert "Добавлен класс для обработки свайпов"
    This reverts commit fa601a75ca3b3ba81838d2f032c5ad339535083d.

--- app/src/main/java/com/journaldev/mvpdagger2/myVIew/ImageViewTouchViewPager.java
+++ app/src/main/java/com/journaldev/mvpdagger2/myVIew/ImageViewTouchViewPager.java
@@ -3,17 +3,13 @@ package com.journaldev.mvpdagger2.myVIew;
 import android.content.Context;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.journaldev.mvpdagger2.Data.AppPreference;
-
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class ImageViewTouchViewPager extends android.support.v4.view.ViewPager {
-
 
     public ImageViewTouchViewPager(Context context) {
         super(context);
@@ -26,7 +22,7 @@ public class ImageViewTouchViewPager extends android.support.v4.view.ViewPager {
     @Override
     protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
         if (v instanceof ImageViewTouch) {
-            ImageViewTouch imageViewTouch = (ImageViewTouch) v;
+            ImageViewTouch imageViewTouch = (ImageViewTouch)v;
             if (imageViewTouch.getScale() == imageViewTouch.getMinScale()) {
                 return super.canScroll(v, checkV, dx, x, y);
             }
@@ -36,8 +32,7 @@ public class ImageViewTouchViewPager extends android.support.v4.view.ViewPager {
         }
     }
 
-
-    private boolean imageViewTouchCanScroll(ImageViewTouch v, int direction) {
+    private boolean imageViewTouchCanScroll(ImageViewTouch v, int direction){
         RectF bitmapRect = v.getBitmapRect();
         Rect imageViewRect = new Rect();
         getGlobalVisibleRect(imageViewRect);
@@ -48,7 +43,7 @@ public class ImageViewTouchViewPager extends android.support.v4.view.ViewPager {
 
         if (direction < 0) {
             return Math.abs(bitmapRect.right - imageViewRect.right) > 1.0f;
-        } else {
+        }else {
             return Math.abs(bitmapRect.left - imageViewRect.left) > 1.0f;
         }
 
