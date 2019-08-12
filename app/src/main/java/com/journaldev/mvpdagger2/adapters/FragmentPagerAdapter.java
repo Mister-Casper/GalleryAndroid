061ca72b4f484fdb2a5c9ff6db33837864c20958
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-12 21:20:40 +0700

    Добавлена вкладка с настройками приложения

--- app/src/main/java/com/journaldev/mvpdagger2/adapters/FragmentPagerAdapter.java
+++ app/src/main/java/com/journaldev/mvpdagger2/adapters/FragmentPagerAdapter.java
@@ -1,22 +1,26 @@
 package com.journaldev.mvpdagger2.adapters;
 
-import android.content.Context;
+
+
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 
+
+import com.journaldev.mvpdagger2.fragments.PreferenceScreen;
 import com.journaldev.mvpdagger2.fragments.ViewAllImagesByDate;
 import com.journaldev.mvpdagger2.fragments.albums;
 
 public class FragmentPagerAdapter extends FragmentStatePagerAdapter {
     int mNumOfTabs;
-    Fragment[] fragments = new Fragment[2];
+    Fragment[] fragments = new Fragment[3];
 
     public FragmentPagerAdapter(FragmentManager fm, int NumOfTabs) {
         super(fm);
         this.mNumOfTabs = NumOfTabs;
         fragments[0] = new ViewAllImagesByDate();
         fragments[1] = new albums();
+        fragments[2] = new PreferenceScreen();
     }
 
     @Override
