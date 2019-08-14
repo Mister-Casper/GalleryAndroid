e01c217e18f9044871534c8b23cacc44dd54e6d4
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-14 17:38:44 +0700

    Добавлена возможность сохранения темы приложения

--- app/src/main/java/com/journaldev/mvpdagger2/Data/AppPreference.java
+++ app/src/main/java/com/journaldev/mvpdagger2/Data/AppPreference.java
@@ -1,13 +1,15 @@
 package com.journaldev.mvpdagger2.Data;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.v7.preference.PreferenceManager;
 
 public class AppPreference {
 
     public static Boolean getIsAnim() {
-        if(isAnim == null)
+        if (isAnim == null)
             throw new IllegalArgumentException();
 
         return isAnim;
@@ -17,15 +19,49 @@ public class AppPreference {
         AppPreference.isAnim = isAnim;
     }
 
+    public static Boolean getIsDarkTheme() {
+        if (isDarkTheme == null)
+            throw new IllegalArgumentException();
+
+        return isDarkTheme;
+    }
+
+    public static void setIsDarkTheme(Boolean isDarkTheme) {
+        AppPreference.isDarkTheme = isDarkTheme;
+    }
+
+    private static Boolean isDarkTheme = null;
     private static Boolean isAnim = null;
 
     public static void load(Context context) {
         SharedPreferences prefs = PreferenceManager
                 .getDefaultSharedPreferences(context);
 
+        loadIsAnim(prefs);
+        loadIsDarkTheme(prefs);
+    }
+
+    private static void loadIsAnim(SharedPreferences prefs) {
         isAnim = prefs.getBoolean("isAnim",
                 true);
     }
 
+    private static void loadIsDarkTheme(SharedPreferences prefs) {
+        isDarkTheme = prefs.getBoolean("isDarkTheme",
+                false);
+    }
 
+    public static int chandgeTheme(Activity activity, int darkTheme, int lightTheme) {
+        if (isDarkTheme == null)
+            activity.setTheme(lightTheme);
+
+        if (isDarkTheme) {
+            activity.setTheme(darkTheme);
+            return darkTheme;
+        }
+        else
+            activity.setTheme(lightTheme);
+
+        return lightTheme;
+    }
 }
