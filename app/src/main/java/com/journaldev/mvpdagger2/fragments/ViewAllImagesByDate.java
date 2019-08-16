d6b959c4684fb73dc88dbc28c77b156b83f24e04
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-08-16 21:20:17 +0700

    Добавлена поддержка использования двух режимов

--- app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate.java
+++ app/src/main/java/com/journaldev/mvpdagger2/fragments/ViewAllImagesByDate.java
@@ -1,8 +1,10 @@
 package com.journaldev.mvpdagger2.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,30 +12,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.journaldev.mvpdagger2.Data.ImageUrls;
 import com.journaldev.mvpdagger2.Data.ItemPhotoData;
+import com.journaldev.mvpdagger2.Data.SelectableItemPhotoData;
 import com.journaldev.mvpdagger2.R;
 import com.journaldev.mvpdagger2.activity.ViewImagesActivity;
 import com.journaldev.mvpdagger2.adapters.PhotosAdapter;
+import com.journaldev.mvpdagger2.adapters.SelectableViewHolder;
 import com.journaldev.mvpdagger2.utils.FabricEvents;
 import com.journaldev.mvpdagger2.utils.MeasurementLaunchTime;
-
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
-public class ViewAllImagesByDate extends Fragment implements PhotosAdapter.ItemClickListener {
+public class ViewAllImagesByDate extends Fragment implements SelectableViewHolder.OnItemClickListener, SelectableViewHolder.OnItemSelectedListener {
 
 
     @BindView(R.id.DataList)
     RecyclerView DataList;
     Unbinder unbinder;
     LinkedList<ItemPhotoData> uri = null;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.exitButton)
+    Button exitButton;
+    @BindView(R.id.itemSelected)
+    TextView itemSelected;
+    @BindView(R.id.deleteItemsSelected)
+    Button deleteItemsSelected;
+    @BindView(R.id.selectablemenu)
+    ConstraintLayout selectablemenu;
+    PhotosAdapter adapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,10 +61,18 @@ public class ViewAllImagesByDate extends Fragment implements PhotosAdapter.ItemC
     public void onStart() {
         super.onStart();
         setAdapter();
+        if (unbinder != null)
+            viewStandartMod();
+
+    }
+
+    private void viewStandartMod() {
+        selectablemenu.setVisibility(View.GONE);
+        textView.setVisibility(View.VISIBLE);
     }
 
     private void setAdapter() {
-        PhotosAdapter adapter = new PhotosAdapter(getActivity().getApplicationContext(), loadUri());
+        adapter = new PhotosAdapter(getActivity().getApplicationContext(), loadUri(), this);
         adapter.setClickListener(this);
         DataList.setAdapter(adapter);
         DataList.invalidate();
@@ -57,7 +82,7 @@ public class ViewAllImagesByDate extends Fragment implements PhotosAdapter.ItemC
         uri = ImageUrls.getUrls(getContext());
         ArrayList<ItemPhotoData> photo = new ArrayList<>();
         for (int i = 0; i < uri.size(); i++)
-            photo.add(new ItemPhotoData(uri.get(i).getPhoto(),uri.get(i).getLike()));
+            photo.add(new ItemPhotoData(uri.get(i).getPhoto(), uri.get(i).getLike()));
         return photo;
     }
 
@@ -67,17 +92,18 @@ public class ViewAllImagesByDate extends Fragment implements PhotosAdapter.ItemC
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragmentviewallimagesbydate, container, false);
         unbinder = ButterKnife.bind(this, view);
+        viewStandartMod();
         setAdapter();
         DataList.setLayoutManager(new GridLayoutManager(getContext(), 4));
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             writeStartTime(container);
         }
 
         return view;
     }
 
-    private void writeStartTime(final View container){
+    private void writeStartTime(final View container) {
         container.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -99,6 +125,26 @@ public class ViewAllImagesByDate extends Fragment implements PhotosAdapter.ItemC
         Intent intent = new Intent(getContext(), ViewImagesActivity.class);
         intent.putExtra("idImage", position);
         getContext().startActivity(intent);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onItemSelected(SelectableItemPhotoData item) {
+        ArrayList<SelectableItemPhotoData> selectedItems = adapter.getSelectedItems();
+        selectablemenu.setVisibility(View.VISIBLE);
+        itemSelected.setText(Integer.toString(selectedItems.size()));
+        textView.setVisibility(View.GONE);
+    }
+
+
+    @OnClick(R.id.exitButton)
+    public void exitButtonClick() {
+        adapter.setSelectable(false);
+    }
+
+    @OnClick(R.id.exitButton)
+    public void deleteItemsSelectedClick() {
+
     }
 
 }
