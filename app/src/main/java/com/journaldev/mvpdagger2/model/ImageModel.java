8937be9e04d0e483f26b1e183f57918245fbf2f7
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-11-29 21:03:03 +0700

    имплиментирован Parcelable

--- app/src/main/java/com/journaldev/mvpdagger2/model/ImageModel.java
+++ app/src/main/java/com/journaldev/mvpdagger2/model/ImageModel.java
@@ -1,11 +1,31 @@
 package com.journaldev.mvpdagger2.model;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class ImageModel {
+public class ImageModel implements Parcelable {
 
     private Uri photo;
     private Boolean isLike;
+
+    protected ImageModel(Parcel in) {
+        photo = in.readParcelable(Uri.class.getClassLoader());
+        byte tmpIsLike = in.readByte();
+        isLike = tmpIsLike == 0 ? null : tmpIsLike == 1;
+    }
+
+    public static final Creator<ImageModel> CREATOR = new Creator<ImageModel>() {
+        @Override
+        public ImageModel createFromParcel(Parcel in) {
+            return new ImageModel(in);
+        }
+
+        @Override
+        public ImageModel[] newArray(int size) {
+            return new ImageModel[size];
+        }
+    };
 
     public Uri getPhoto() {
         return photo;
@@ -21,5 +41,16 @@ public class ImageModel {
     public ImageModel(Uri photo , Boolean isLike) {
         this.photo = photo;
         this.isLike = isLike;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeParcelable(photo, i);
+        parcel.writeByte((byte) (isLike == null ? 0 : isLike ? 1 : 2));
     }
 }
