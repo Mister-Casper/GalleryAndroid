bcc598ac30e2f9e36a60b073d427135aa2fc9a75
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-11-29 21:03:54 +0700

    Добавлено активити для просмотра изображений из одного альбома

---
+++ app/src/main/java/com/journaldev/mvpdagger2/view/activity/ViewImagesGridActivity.java
@@ -0,0 +1,34 @@
+package com.journaldev.mvpdagger2.view.activity;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.journaldev.mvpdagger2.R;
+import com.journaldev.mvpdagger2.utils.ThemeUtils;
+import com.journaldev.mvpdagger2.view.fragment.BaseGridImages;
+
+
+
+public class ViewImagesGridActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ThemeUtils.changeTheme(this, R.style.DarkTheme2, R.style.LightTheme2);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_images_grid);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment baseGridImages = new BaseGridImages();
+        Bundle images = getIntent().getExtras();
+        baseGridImages.setArguments(images);
+        fragmentTransaction.add(R.id.imagesFragment,baseGridImages);
+        fragmentTransaction.commit();
+    }
+
+
+}
