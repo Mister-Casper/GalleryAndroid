77c0607134da07c8e03c5286c5f17e82a1c91511
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-11-30 16:49:15 +0700

    Update SelectableAlbumModel.java

--- app/src/main/java/com/journaldev/mvpdagger2/model/SelectableAlbumModel.java
+++ app/src/main/java/com/journaldev/mvpdagger2/model/SelectableAlbumModel.java
@@ -1,11 +1,13 @@
 package com.journaldev.mvpdagger2.model;
 
+import java.util.ArrayList;
+
 public class SelectableAlbumModel extends AlbumModel {
 
     private boolean isSelected;
 
     public SelectableAlbumModel(AlbumModel item, boolean isSelected) {
-        super(item.getName(),item.getUri(),item.getLike());
+        super(item.getName(), item.getUri(), item.getLike());
         this.isSelected = isSelected;
     }
 
@@ -16,4 +18,20 @@ public class SelectableAlbumModel extends AlbumModel {
     public void setSelected(boolean selected) {
         isSelected = selected;
     }
+
+    public static String[] convertAlbumsToStringArray(ArrayList<SelectableAlbumModel> albums) {
+        ArrayList<String> urls = new ArrayList<>();
+        String[] urlArray = new String[urls.size()];
+
+        for (int i = 0; i < albums.size(); i++) {
+            ArrayList<ImageModel> images = albums.get(i).getImages();
+            for (int q = 0; q < images.size(); q++) {
+                urls.add(images.get(q).getPhoto().toString());
+            }
+        }
+
+        return urls.toArray(urlArray);
+    }
+
+
 }
