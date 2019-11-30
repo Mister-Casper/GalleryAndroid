a67993a470ac21e242d12ebb291f518112b81c11
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-11-30 16:49:18 +0700

    Update SelectableImageModel.java

--- app/src/main/java/com/journaldev/mvpdagger2/model/SelectableImageModel.java
+++ app/src/main/java/com/journaldev/mvpdagger2/model/SelectableImageModel.java
@@ -1,6 +1,8 @@
 package com.journaldev.mvpdagger2.model;
 
 
+import java.util.ArrayList;
+
 public class SelectableImageModel extends ImageModel {
 
     private boolean isSelected;
@@ -16,5 +18,15 @@ public class SelectableImageModel extends ImageModel {
 
     public void setSelected(boolean selected) {
         isSelected = selected;
+    }
+
+    public static String[] convertImagesToStringArray(ArrayList<SelectableImageModel> uri) {
+        String[] str = new String[uri.size()];
+
+        for (int i = 0; i < str.length; i++) {
+            str[i] = uri.get(i).getPhoto().toString();
+        }
+
+        return str;
     }
 }
