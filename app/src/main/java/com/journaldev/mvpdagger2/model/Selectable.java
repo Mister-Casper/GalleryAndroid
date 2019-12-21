b38103de677a5f865d1f432b769e02398cbc49f7
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-12-21 09:38:25 +0700

    Добавлен метод для получения uri из массива выделенных элементов

--- app/src/main/java/com/journaldev/mvpdagger2/model/Selectable.java
+++ app/src/main/java/com/journaldev/mvpdagger2/model/Selectable.java
@@ -8,4 +8,14 @@ public interface Selectable {
 
     ArrayList<ImageModel> getImages();
 
+    static  ArrayList<Uri> getAll(ArrayList<Selectable> selectedItems) {
+        ArrayList<Uri> images = new ArrayList<>();
+        for (int i = selectedItems.size()-1; i >=0; i--) {
+            ArrayList<ImageModel> selectImages = selectedItems.get(i).getImages();
+            for (int q = selectImages.size()-1; q >=0; q--) {
+                images.add(selectedItems.get(i).getImages().get(q).getImage());
+            }
+        }
+        return images;
+    }
 }
