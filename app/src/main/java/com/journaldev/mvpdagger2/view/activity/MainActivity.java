9deea34606e6abc33892cad4d263d96486e9c883
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-12-30 19:56:42 +0700

    Исправлен баг

--- app/src/main/java/com/journaldev/mvpdagger2/view/activity/MainActivity.java
+++ app/src/main/java/com/journaldev/mvpdagger2/view/activity/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void isToSettings() {
-        if (getIntent().getBooleanExtra("isOptionsChandge", false))
+        if (getIntent().getBooleanExtra("isOptionsChange", false))
             toSettings();
     }
 
