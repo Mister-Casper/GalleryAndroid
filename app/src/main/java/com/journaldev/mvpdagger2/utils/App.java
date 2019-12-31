3acd31adbb4ee71f8a5d4b26790dd99f7c579a93
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-12-31 17:03:36 +0700

    Добавлен класс для работы с настройкамиприложения

--- app/src/main/java/com/journaldev/mvpdagger2/utils/App.java
+++ app/src/main/java/com/journaldev/mvpdagger2/utils/App.java
@@ -4,20 +4,20 @@ import android.app.Application;
 
 public class App extends Application {
     private static App app;
-    private static ImageHelper imageHelper;
+    private static AppPreference appPreference;
 
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
-        imageHelper = new ImageHelper(getApplicationContext());
-    }
-
-    public static ImageHelper getImageHelper() {
-        return imageHelper;
+        appPreference = new AppPreference(app);
     }
 
     public static App getApp() {
         return app;
+    }
+
+    public static AppPreference getAppPreference() {
+        return appPreference;
     }
 }
