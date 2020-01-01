02c863e71dc9e05d4f885add13e844ffea5bb855
Kirill Kozinets <kirillkozinec123@gmail.com>
2020-01-01 23:35:25 +0700

    Refactoring

--- app/src/main/java/com/journaldev/mvpdagger2/data/Album/AlbumRepositoryObserver.java
+++ app/src/main/java/com/journaldev/mvpdagger2/data/Album/AlbumRepositoryObserver.java
@@ -1,11 +1,7 @@
 package com.journaldev.mvpdagger2.data.Album;
 
 import com.journaldev.mvpdagger2.model.AlbumModel;
-import com.journaldev.mvpdagger2.model.ImageModel;
-import com.journaldev.mvpdagger2.model.Selectable.AlbumSelectable;
-
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public interface AlbumRepositoryObserver {
 
